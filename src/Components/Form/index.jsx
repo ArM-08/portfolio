f7b00e9f169270d251040827e3a74770a4c2e8c4
ArM-08 <62227850+ArM-08@users.noreply.github.com>
2023-02-01 16:02:38 -0300

    resetForm

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,92 +1,36 @@
-
-import { useForm } from "react-hook-form"
-import styled from "styled-components"
-import emailjs from "@emailjs/browser"
-
-
-
-const FormContainer = styled.div`
-border-radius: 50px;
-width: 600px;
-background: #000;
-height: 600px;
-display: flex;
-justify-content: center;
-align-items: center;
-flex-wrap: wrap;
-flex-direction: column;
-`
-
-const Title = styled.h1`
-color: #FFF;
-`
-
-const FormInput = styled.form`
-display: flex;
-flex-direction: column;
-`
-
-const Input = styled.input`
-width: 400px;
-height: 40px;
-margin: 10px;
-`
-const Mensagem = styled.textarea`
-width: 400px;
-height: 100px;
-margin: 10px;
-`
-const Label = styled.label`
-color: #FFF;
-`
-
-const Button = styled.button`
-width: 150px;
-height: 40px;
-align-self: center;
-`
-
-
-
+import { FormContainer, Title, FormInput, Label, Input, Mensagem, Button  } from './styled';
+import { useForm, ValidationError } from '@formspree/react';
+import { useEffect, useRef} from "react";
+import React from 'react';
 
 const Form = () => {
 
-    const {register, handleSubmit} = useForm()
 
-    const onSubmit = (data, e) => {
+    const [state, handleSubmit] = useForm("mrgveyky")
+    const formRef = useRef(null)
+ useEffect(() => {
+   
+    if(state.succeeded)
+    console.log('foi')
+        formRef.current.reset();
+      
+ })
 
-       /* tirar comments para ativar serviço emailJS
-
-
-       const TemplateParams = {
-        from_nome: data.nome,
-        message: data.mensagem,
-        email: data.email
-           }
-          emailjs.send("service_hw0fon5", "template_hed8gxq", TemplateParams, "QVmEs4h7FwC5nD5ne" )
-           .then((response)=> {
-               //Aplicar modal Sucess
-               e.target.reset()
-               console.log("enviado com sucesso", response.status, response.text)
-           }, (err) => {
-               //Aplicar modal error 
-               console.log("Deu error", err)
-           })
-       */ 
-    }
-
-    
     return (
         <FormContainer>  
             <Title>Email</Title>
-            <FormInput onSubmit={handleSubmit(onSubmit)}>
+            <FormInput ref={formRef} onSubmit={handleSubmit}>
                 <Label>Nome</Label>
-                <Input {...register('nome')} type="text" placeholder="Nome" required />
+                <Input name="nome" type="text" id="nome" placeholder="Nome" required/>
+                <ValidationError prefix="Nome" field="nome" errors={state.errors}/>
                 <Label>Email</Label>
-                <Input  {...register('email')} type="email" placeholder="meuemail@email.com" required></Input>
+                <Input  name="email" type="email" id="email" placeholder="meuemail@email.com" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" ></Input>
+<ValidationError prefix="Email" field="email" errors={state.errors}/>
                 <Label>Mensagem</Label>
-                <Mensagem  {...register('mensagem')} type="textarea" placeholder="Mensagem" required></Mensagem>
-                <Button type="submit">Enviar</Button>
+                <Mensagem  name="mensagem" type="textarea" id="mensagem"placeholder="Mensagem" required></Mensagem>
+                <ValidationError prefix="Mensagem" field="mensagem" errors={state.errors}/>
+                <Button type="submit" disabled={state.submitting}>Enviar</Button>
+                <ValidationError errors={state.errors} />
             </FormInput>
             </FormContainer>
     )
---
+++ src/Components/Form/styled.jsx
@@ -0,0 +1,46 @@
+
+import styled from "styled-components"
+
+export const FormContainer = styled.div`
+border-radius: 50px;
+width: 600px;
+background: #000;
+height: 600px;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-wrap: wrap;
+flex-direction: column;
+`
+
+export const Title = styled.h1`
+color: #FFF;
+`
+
+export const FormInput = styled.form`
+display: flex;
+flex-direction: column;
+`
+
+export const Input = styled.input`
+width: 400px;
+height: 40px;
+margin: 10px;
+`
+export const Mensagem = styled.textarea`
+width: 400px;
+height: 100px;
+margin: 10px;
+`
+export const Label = styled.label`
+color: #FFF;
+`
+
+export const Button = styled.button`
+width: 150px;
+height: 40px;
+align-self: center;
+`
+
+
+
