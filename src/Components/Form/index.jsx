3675c1b3ce1ff8e950d029b3b25bc59df6cd9978
ArM-08 <62227850+ArM-08@users.noreply.github.com>
2023-02-07 12:52:57 -0300

    change service Form for emailJS

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,45 +1,59 @@
 import { FormContainer, Title, FormInput, Label, Input, Mensagem, Button } from './styled';
-import { useForm, ValidationError } from '@formspree/react';
-import { useEffect, useRef, useState} from "react";
+import {useState} from "react";
 import React from 'react';
 import ToastMessage from "./../ToastMessage"
-
+import emailjs from "@emailjs/browser"
 
 
 
 
 const Form = () => {
 
+const timeToast = () =>{
+    setTimeout(() => {
+        openToast(false);
+    }, 2000)
+}
 const [toast, openToast] = useState(false)
-    const [state, handleSubmit] = useForm("mrgveyky")
-    const formRef = useRef(null)
+const [name, setName] = useState('')
+const [email, setEmail] = useState('')
+const [message, setMessage]= useState('')
+const handleSubmit = (e) => {
+    e.preventDefault()
+const templateParams = {
+    from_name: name,
+    email: email,
+    message: message
+}
+    {
+        emailjs.send("service_hw0fon5", "template_hed8gxq", templateParams, "QVmEs4h7FwC5nD5ne")
+        .then((response) => {
+            openToast(true)
+            setName('')
+            setEmail('')
+            setMessage('')
+            timeToast()
+            
+            
+        }, (err)=> {
+            console.log("error")
+        })
+    }
 
- useEffect(() => {
-   
-    if(state.succeeded){
-    openToast(true)
-    setTimeout(()=> {
-        openToast(false);  
-    }, 2000);
-} 
-formRef.current.reset();
- },)
-
+}
     return (
         <FormContainer>  
             <Title>Email</Title>
-            <FormInput ref={formRef} onSubmit={handleSubmit}>
+            <FormInput onSubmit={handleSubmit}>
                 <Label>Nome</Label>
-                <Input name="nome" type="text" id="nome" placeholder="Nome" required/>
-                <ValidationError prefix="Nome" field="nome" errors={state.errors}/>
+                <Input name="nome" type="text" id="name" placeholder="Nome" required onChange={(e) => setName(e.target.value)} value={name}/>
                 <Label>Email</Label>
-                <Input  name="email" type="email" id="email" placeholder="meuemail@email.com" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" ></Input>
-<ValidationError prefix="Email" field="email" errors={state.errors}/>
+                <Input  name="email" type="email" id="email" placeholder="meuemail@email.com" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" onChange={(e) => setEmail(e.target.value)} value={email}></Input>
                 <Label>Mensagem</Label>
-                <Mensagem  name="mensagem" type="textarea" id="mensagem"placeholder="Mensagem" required></Mensagem>
-                <ValidationError prefix="Mensagem" field="mensagem" errors={state.errors}/>
-                <Button type="submit" disabled={state.submitting}>Enviar</Button>
-                <ValidationError errors={state.errors} />
+                <Mensagem  name="mensagem" type="textarea" id="mensagem"placeholder="Mensagem" required onChange={(e) => setMessage(e.target.value)} value={message}></Mensagem>
+
+                <Button type="submit">Enviar</Button>
+
             </FormInput>
             {toast && <ToastMessage/>}
             </FormContainer>
